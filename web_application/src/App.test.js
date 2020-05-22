1b039681a23e522f8519ee437182ee577b0ecf38
Jesse Wood <j.r.h.wood98@gmail.com>
2020-05-22 23:29:10 +1200

    Tests all text for the apge

--- web_application/src/App.test.js
+++ web_application/src/App.test.js
@@ -2,14 +2,26 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-// This test will always pass
-test('Fake test', () => {
-  expect(true).toBeTruthy();
-})
+test('renders Mission Control title', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Mission Control/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
-// Checks there is a link with the name Repository 
 test('renders Repository link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Repository/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders a template text', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/A template/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Mission-Control-System inline code', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Mission-Control-System/i);
   expect(linkElement).toBeInTheDocument();
 });
