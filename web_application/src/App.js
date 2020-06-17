9280a89c4039a6e48773df7b0a4f18dd8355fb5c
Jesse Wood <j.r.h.wood98@gmail.com>
2020-06-17 18:01:56 +1200

    Wrote HTML for landing page

--- web_application/src/App.js
+++ web_application/src/App.js
@@ -6,20 +6,42 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Mission Control</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          A template <code>Mission-Control-System</code>
-        </p>
+      </header>
+      <div className="App-body">
+        <h1>Mission Control</h1>
+        <div className="App-description">
+          The mission control runs on a laptop in the field and displays data from the rocket. It can let the rocket know when the launch is immiment so the avionics can arm the parachute ejection charges. Mission control has a go / no go functionality, and integration with a Monte Carlo rocket simulation.
+        </div>
+        <p></p>
         <a
-          className="App-link"
+          className="App-link App-begin"
           href="https://gitlab.ecs.vuw.ac.nz/course-work/engr300/2020/group12/group-12"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Repository
+          Begin
         </a>
-      </header>
+        <a
+          className="App-link App-instructions"
+          href="https://gitlab.ecs.vuw.ac.nz/course-work/engr300/2020/group12/group-12"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Instructions
+        </a>
+      </div>
+      <footer>
+        <a
+          className="App-link App-code"
+          href="https://gitlab.ecs.vuw.ac.nz/course-work/engr300/2020/group12/group-12"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View Source
+          <i class="material-icons">code</i>
+        </a>
+      </footer>
     </div>
   );
 }
