6950b7291745567e9ce3a90374e6d5b6b55d9e45
Binke Xu <xubink@ecs.vuw.ac.nz>
2020-07-28 10:47:55 +1200

    save button works, need to work out how to save data as JSON file.

--- web_application/src/components/Weather/Weather.js
+++ web_application/src/components/Weather/Weather.js
@@ -1,23 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './weather.css'
 
 /**
  *  This page can retrive the current weather conditions.
  *  We use the OpenWeatherMap API, which required an internet connection.
  *  The user can save and load weather forecasts for offline usage.
  */
-const Weather = props => {
+
+
+function Weather() {
+
+  function saveData() {
+    var jsonData = '{"wellington":[{"weather":"sunny"},{"wind-speed":"5m/s"}]}';
+
+    // parse json
+    var jsonParsed = JSON.parse(jsonData);
+
+    var jsonString = JSON.stringify(jsonParsed);
+
+    console.log(jsonString);
+
+  }
+
   return (
     <div>
       <Link className="Instructions-back" to="/"><i className="material-icons">navigate_before</i></Link>
       <div className="Landing-body">
         <h1>Weather</h1>
-        <b>
-           <Link to="/SaveData">Save Data</Link>
-       </b>
+
+        <button onClick={saveData}>
+          Save
+        </button>
+
       </div>
     </div>
   );
+
 }
 
 export default Weather;
