27695cded3642b2ac4a8b1a5bef394e75819c5fd
Marcus Rathod <rathodmarc@ecs.vuw.ac.nz>
2020-07-28 12:03:20 +1200

    current weather implementation works in conjunction with the web application

--- web_application/src/components/Weather/Weather.js
+++ web_application/src/components/Weather/Weather.js
@@ -7,9 +7,57 @@ function Weather() {
       <Link className="Instructions-back" to="/"><i className="material-icons">navigate_before</i></Link>
       <div className="Landing-body">
         <h1>Weather</h1>
+        <document>
+          <p id="weatherdata">Please enter the required fields below</p>
+          
+          <label for="space"> </label><br></br>
+          
+          <label for="latValue">Latitude:</label><br></br>
+          <input type="int" placeholder="e.g. -41.2769" id="latId"></input><br></br>
+
+          <label for="space"> </label><br></br>
+          
+          <label for="longValue">Longitude:</label><br></br>
+          <input type="int" placeholder="e.g. 174.7731" id="lonId"></input>
+          
+        </document>
+        &nbsp;
+        <button type="button" id="weatherButton" onClick={getJson}>Get Weather Data</button>
+        {/* <script type="text/javascript">
+          function(){
+            getJson()
+          }
+        </script> */}
+
       </div>
     </div>
-  );
+  ); 
 }
+
+function getJson(){
+  document.getElementById("weatherdata").style.border = "thin dotted white";
+
+  var lat = document.getElementById("latId").value;
+  var lon = document.getElementById("lonId").value;
+
+  var URL = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&APPID=9dbce15b12f75333f8f274f51793d0ff&mode=json&units=metric" //mode=html should be used for dashboard
+  
+  var Httpreq = new XMLHttpRequest(); 
+
+  Httpreq.open("GET",URL,false);
+  Httpreq.send(null);
+
+  var json = JSON.parse(Httpreq.responseText);
+  
+
+  document.getElementById("weatherdata").innerHTML = "Current weather for: *" + json.name + 
+  "<br>Humidity: "+ json.main.humidity + "%" +
+  "<br>Wind Speed: "+ json.wind.speed + " m/s" +
+  "<br>Wind Direction: "+ (json.wind.deg*(Math.PI/180)) + " radians" +
+  "<br>Temperature: "+ json.main.temp + "°C" + //Beware of this symbol, may cause problems when packaging
+  "<br>Air Pressure: "+ json.main.pressure + " hPa" +
+  "<br><br><br>*Approximate Location"; 
+}
+
 
 export default Weather;
