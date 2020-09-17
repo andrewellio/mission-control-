10c7e7e684cf4f3a2cb7ccda91ad7cf80c9d2788
Jesse Wood <j.r.h.wood98@gmail.com>
2020-09-17 18:22:35 +1200

    Refactor the `App.js` to stay DRY

--- web_application/src/App.js
+++ web_application/src/App.js
@@ -15,29 +15,25 @@ import Weather from "./pages/Weather";
  *  This page house the single page application (SPA).
  *  We use the DOM to control routing between pages.
  */
-const App = () => {
+export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/Dashboard">
-          <Dashboard />
-        </Route>
-        <Route path="/Instructions">
-          <Instructions />
-        </Route>
-        <Route path="/Weather">
-          <Weather />
-        </Route>
-        <Route path="/Simulation">
-          <Simulation />
-        </Route>
-        <Route path="/Home">
-          <Landing />
-        </Route>
-        <Redirect from="/" to="/Home" />
+        {pages.map(page => (
+          <Route path={page.path} component={page.component} />
+        ))}
+        <Redirect from="/" to={defaultPage} />
       </Switch>
     </Router>
   );
-};
+}
 
-export default App;
+const pages = [
+  { path: "/Home", component: Landing },
+  { path: "/Dashboard", component: Dashboard },
+  { path: "/Instructions", component: Instructions },
+  { path: "/Weather", component: Weather },
+  { path: "/Simulation", component: Simulation }
+];
+
+const defaultPage = pages[0].path;
