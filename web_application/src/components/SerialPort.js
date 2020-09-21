e99444fa24dde21a53a444fdf1349f6c930c527e
Binke Xu <xubink@ecs.vuw.ac.nz>
2020-09-22 11:11:47 +1200

    The code can read and write data.

--- web_application/src/components/SerialPort.js
+++ web_application/src/components/SerialPort.js
@@ -1,24 +1,30 @@
-import weatherData from "../data/WeatherData.json";
-import React, { useState } from "react";
-const [imcomeData,setIncomeData] = useState();  //to store the data from SerialPort
 const SerialPort = require('serialport');  //npm i serialport
 const Readline = require('@serialport/parser-readline')  //npm i @serialport/parser-readline
 
-const port = new SerialPort('COM1',{  //not sure if this is correct
-    baudrate: 9600,
-  });
+const port = new SerialPort('COM3', {  //not sure if this is correct
+  baudRate: 9600,
+  autoOpen: true
+});
 
+const recievedData = 'data';
 // The Serial port parser
 const parser = new Readline();
 port.pipe(parser);
 
 // Read the data from the serial port
-parser.on("data", (line) => console.log(line),setIncomeData(line));  //log data and store it
+parser.on("data", (line) => parseData(line));  //log data and store it
 
-// Write the data to the serial port
-port.write("Some date write to port!");  // can change to weatherData
+// Write the data to the serial port, can change to weatherData
+writeMessage("hello")
+console.log("data to port")
 
-function writeMessage() {
+function writeMessage(data) {
   let msg = "date write to port!";
-  port.write(msg)
+  port.write(data)
+}
+
+function parseData(data){
+  let msg = data;
+  recievedData = data
+  console.log(recievedData);
 }
