250b13965ad0f54d80539d7472514a25e0710f18
Mason Yi <MasonYi@Masons-MacBook-Pro-2.local>
2020-10-02 00:45:42 +1300

    Implemented basic graph to display on screen.

--- web_application/src/pages/Graphs.js
+++ web_application/src/pages/Graphs.js
@@ -1,13 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Plot from 'react-plotly.js';
 
 
 /**
  *  This page displays the graphs using the data from the serial usb port.
  */
 function Graphs() {
+
+
+  var data = [{
+    x: [1, 2, 3, 4],
+    y: [2, 4, 6, 8],
+    type: 'scatter',
+    mode: 'lines+markers',
+    marker: {color: 'red'},
+  }];
+  
+  var layout = {
+    width: '50%', height: '50%', title: 'Test Graph'
+  };
+
+
+
+
+
   return (
-    null
+    
+    <div>
+      <Link className="Instructions-back" to="/"><i className="material-icons">navigate_before</i></Link>
+      <div className="Landing-body">
+        <h1>Graphs</h1>
+      <Plot
+        data={data}
+        layout={layout}
+      />      
+      </div>
+    </div>
   );
 }
 
