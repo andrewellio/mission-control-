d86d8857d8823aefce18fcd3b4445c1741e73f15
Mason Yi <MasonYi@Masons-MacBook-Pro-2.local>
2020-10-08 22:29:51 +1300

    Adds data through button for now, won't update graph though.

--- web_application/src/pages/Graphs.js
+++ web_application/src/pages/Graphs.js
@@ -1,40 +1,59 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Plot from 'react-plotly.js';
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Plot from "react-plotly.js";
 
 /**
  *  This page displays the graphs using the data from the serial usb port.
  */
 function Graphs() {
+  const [data, setData] = useState([
+    {
+      x: [1, 2, 3, 4],
+      y: [1, 2, 3, 4],
+      type: "scatter",
+      mode: "lines+markers",
+      marker: { color: "red" },
+    },
+  ]);
 
-
-  var data = [{
-    x: [1, 2, 3, 4],
-    y: [2, 4, 6, 8],
-    type: 'scatter',
-    mode: 'lines+markers',
-    marker: {color: 'red'},
-  }];
-  
-  var layout = {
-    width: '50%', height: '50%', title: 'Test Graph'
+  const layout = {
+    width: "50%",
+    height: "50%",
+    title: "Test Graph",
   };
 
+  const handleAdd = () => {
+    console.log("data");
+    console.log(data);
 
+    console.log("data[0].x");
+    console.log(data[0].x);
 
+    console.log("data[0].y");
+    console.log(data[0].y);
 
+    const newDataX = Math.round(Math.random() * 10);
+    const newDataY = Math.round(Math.random() * 10);
+
+    data[0].x.push(newDataX);
+    data[0].y.push(newDataY);
+
+    console.log("data[0].x");
+    console.log(data[0].x);
+
+    console.log("data[0].y");
+    console.log(data[0].y);
+  };
 
   return (
-    
     <div>
-      <Link className="Instructions-back" to="/"><i className="material-icons">navigate_before</i></Link>
+      <Link className="Instructions-back" to="/">
+        <i className="material-icons">navigate_before</i>
+      </Link>
       <div className="Landing-body">
         <h1>Graphs</h1>
-      <Plot
-        data={data}
-        layout={layout}
-      />      
+        <Plot data={data} layout={layout} />
+        <button onClick={handleAdd}>Add Data</button>
       </div>
     </div>
   );
